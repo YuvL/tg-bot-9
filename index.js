268b30e2c1e026d80155f45e2e2e880618659293
Yuri Vlasov <you@example.com>
2018-12-29 02:44:48 +0300

    added hello message

--- index.js
+++ index.js
@@ -31,16 +31,51 @@ class PingController extends TelegramBaseController {
      * @param {Scope} $
      */
     pingHandler($) {
-        $.sendMessage('pong')
+        $.sendMessage('pong');
     }
 
+    formHandler($){
+        const form = {
+            name: {
+                q: 'Send me your name',
+                error: 'sorry, wrong input',
+                validator: (message, callback) => {
+                    if(message.text) {
+                        callback(true, message.text) //you must pass the result also
+                        return
+                    }
+
+                    callback(false)
+                }
+            },
+            age: {
+                q: 'Send me your age',
+                error: 'sorry, wrong input',
+                validator: (message, callback) => {
+                    if(message.text && IsNumeric(message.text)) {
+                        callback(true, toInt(message.text))
+                        return
+                    }
+
+                    callback(false)
+                }
+            }
+        }
+
+        $.runForm(form, (result) => {
+            console.log(result)
+        })
+    }
+    
     get routes() {
         return {
-            'pingCommand': 'pingHandler'
+            'pingCommand': 'pingHandler',
+            'formCommand': 'formHandler'
         }
     }
 }
 
 tg.router
     .when(new TextCommand('start', 'startCommand'), new GiftController())
-    .when(new TextCommand('ping', 'pingCommand'), new PingController());
+    .when(new TextCommand('ping', 'pingCommand'), new PingController())
+    .when(new TextCommand('form', 'formCommand'), new PingController());
