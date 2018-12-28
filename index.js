077472d7b1343cef3ccb03bb02736612e55a9930
Yuri Vlasov <you@example.com>
2018-12-29 02:13:20 +0300

    added hello message

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+'use strict'
+
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
 const tg = new Telegram.Telegram(process.env.tg_api_key)
+
+class GiftDialog {
+    getHelloMessage() {
+        return process.env._helloMessage;
+    }
+}
+
+class GiftController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    startHandler($) {
+        $.sendMessage( new GiftDialog().getHelloMessage())
+    }
+
+    get routes() {
+        return {
+            'startCommand': 'startHandler'
+        }
+    }
+}
 
 class PingController extends TelegramBaseController {
     /**
@@ -19,4 +42,5 @@ class PingController extends TelegramBaseController {
 }
 
 tg.router
+    .when(new TextCommand('start', 'startCommand'),  GiftController())
     .when(new TextCommand('ping', 'pingCommand'),  PingController());
